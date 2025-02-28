3683fe79e5627df1c1257b25280e3cc7d046edb7
norodondt <131435585+norodondt@users.noreply.github.com>
2025-02-28 14:06:13 +0100

    Structured Data

--- src/navigation.ts
+++ src/navigation.ts
@@ -20,7 +20,7 @@ export const headerData = {
       }
       
   ],
-  actions: [{ text: 'Visit LinkedIn', href: 'https://www.linkedin.com/in/dennistroeger/', target: '_blank' },{ text: 'Watch YouTube', href: 'https://www.youtube.com/@dennis_decodes_systems', target: '_blank', variant: 'primary' }],
+  actions: [{ text: 'Visit LinkedIn', href: 'https://www.linkedin.com/in/dennistroeger', target: '_blank' },{ text: 'Watch YouTube', href: 'https://www.youtube.com/@dennis_decodes_systems', target: '_blank', variant: 'primary' }],
   
 };
 
